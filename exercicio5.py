8581a29ff6444dab5270ac0717a505361a7c8281
davigabrielr <davi.roza@amf.edu.br>
2026-04-14 22:04:36 -0300

    Add simulation for growth of plantations A and B

---
+++ exercicio5.py
@@ -0,0 +1,12 @@
+A = 80000
+B = 200000
+anos = 0
+
+while A < B:
+    A = A + (A * 0.03)
+    B = B + (B * 0.015)
+    anos = anos + 1
+
+print("Anos necessários:", anos)
+print("Plantação A:", A)
+print("Plantação B:", B)
